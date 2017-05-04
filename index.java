af2ac583f37c21616c2017cb85c0acb79e49ac23
lordabdullah <abdullah-chaudhry@hotmail.com>
2017-05-03 22:27:06 -0400

    Commit #2 - Start Alpha Development (0.1)

---
+++ index.java
@@ -0,0 +1,16 @@
+public class index{
+
+  public static void main(String []args) {
+
+        System.out.println("\n                                   Tama OS Alpha 0.1"); // statement to display on start of program
+        System.out.println("Authors: Abdullah Chaudhry, Kavinath Kanesalingam, Ryan Lawson, Vedha Rajan"); //statement to display Authors
+        System.out.println("The following program is confidential and eyes only for the duration of the development period. \n");
+
+
+
+
+
+
+     }
+
+}
